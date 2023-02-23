d147b99c5958d49bbe589340dc9bd0e9e90a4ad0
ZoharBabayof <zzoharbabayof@gmail.com>
2023-02-23 20:14:12 +0200

    yo

--- src/main/java/com/example/trytable2/HelloApplication.java
+++ src/main/java/com/example/trytable2/HelloApplication.java
@@ -7,6 +7,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Side;
+import javafx.print.PageLayout;
+import javafx.print.PrinterJob;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -60,6 +62,10 @@ public class HelloApplication extends Application {
         Patient e7 = new Patient("e3","1",4,5,"Vascular_surgery");
         Patient e8 = new Patient("e4","2",4,5,"Vascular_surgery");
         n.getPatient_queue().add(a);
+        n.getPatient_queue().add(a);
+        n.getPatient_queue().add(a);
+        n.getPatient_queue().add(a);
+
         p.getPatient_queue().add(b);
         c.getPatient_queue().add(f);
         v.getPatient_queue().add(d);
@@ -188,10 +194,11 @@ public class HelloApplication extends Application {
 
         // for vascular surgery
 
-        Iterator<Patient> iterator2 = n.getPatient_queue().iterator();
+        Iterator<Patient> iterator2 = p.getPatient_queue().iterator();
         while(iterator2.hasNext()){
             Patient k = iterator2.next();
             //FileData g = new FileData(q.getName(),q.getId(),q.getSpec_needed(),q.getSpec_needed());
+
             data2.add(k);
             //    FileData l =  new FileData(q.getName(), q.getId(), String.valueOf(q.getSeverity_before()), String.valueOf(q.getWaiting_time()),q.getSpec_needed());
 
@@ -200,27 +207,37 @@ public class HelloApplication extends Application {
         //Adding data to the table
         ObservableList<String> list2 = FXCollections.observableArrayList();
         table2.setItems(data2);
+
         table2.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         table2.getColumns().addAll(NameCol2, IDCol2, waitCol2, sevCol2,specCol2);
         //Setting the size of the table
         table2.setMaxSize(500, 400);
 
-        TabPane tabPane2 = new TabPane();
+
+
         // vbox.
         AnchorPane pane2 = new AnchorPane();
-        AnchorPane.setTopAnchor(tabPane2, 15.0);
-        AnchorPane.setRightAnchor(tabPane2, 15.0);
-        AnchorPane.setBottomAnchor(tabPane2, 15.0);
-        AnchorPane.setLeftAnchor(tabPane2, 15.0);
-        pane2.getChildren().addAll(tabPane2,label2,table2);
+        AnchorPane.setTopAnchor(tabPane, 15.0);
+        AnchorPane.setRightAnchor(tabPane, 15.0);
+        AnchorPane.setBottomAnchor(tabPane, 15.0);
+        AnchorPane.setLeftAnchor(tabPane, 15.0);
+        pane2.getChildren().addAll(tabPane,label2,table2);
         pane2.setStyle("-fx-background-color: BEIGE");
 
+        PrinterJob printerJob = PrinterJob.createPrinterJob();
+        if (printerJob != null) {
+            PageLayout pageLayout = printerJob.getPrinter().createPageLayout(Paper.A5, PageOrientation.LANDSCAPE, 0, 0, 0, 0);
 
-        stage.setTitle("TabPane Demo");
-        BorderPane root2 = new BorderPane();
+            boolean success = printerJob.printPage(pageLayout, root);
+            if (success) {
+                printerJob.endJob();
+            }
+        }
+      //  stage.setTitle("TabPane Demo");
+     //   BorderPane root2 = new BorderPane();
 
-        tabPane = new TabPane();
-        tabPane.setSide(Side.BOTTOM);
+       // tabPane = new TabPane();
+        //tabPane.setSide(Side.BOTTOM);
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -243,7 +260,7 @@ public class HelloApplication extends Application {
         // Button button = new Button("Button" + Integer.toString(i));
         tab.setContent(pane);
         tabPane.getTabs().add(tab);
-        Tab tab1 = new Tab("1");
+        Tab tab1 = new Tab("Neurosurgery");
         // Button button = new Button("Button" + Integer.toString(i));
         tab1.setContent(pane2);
         tabPane.getTabs().add(tab1);
@@ -251,6 +268,8 @@ public class HelloApplication extends Application {
 
 
         root.setCenter(tabPane);
+      //  root.setCenter(tabPane2);
+
 
         Stage stage2 = new Stage();
         Scene scene = new Scene(root, 800, 500);
